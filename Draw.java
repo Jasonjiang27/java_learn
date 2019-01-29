d4081c5fe5cb54147156e5d3129201bb3ba9da6d
Jasonjiang27 <jiangyz1991@163.com>
2019-01-29 23:51:14 +0800

    switch while dowhile

---
+++ Draw.java
@@ -0,0 +1,18 @@
+public class Draw {
+    public static void main(String[] args) {
+	int num = 2;
+	switch(num){
+	case 1:
+	    System.out.println("gongxi yidengjiang");
+ 	    break;
+	case 2:
+	    System.out.println("gongxi erdengjiang");
+	    break;
+	case 3:
+	    System.out.println("gongxi sandengjiang");
+	    break;
+	default:
+	    System.out.println("henyihan");
+	}
+    }
+}
---
+++ SumOfEven.java
@@ -0,0 +1,21 @@
+public class SumOfEven {
+	public static void main(String[] args){
+	    int i1 =1, i2 = 1;
+	    int sum1 = 0, sum2 = 0;
+	    while (i1 <= 1000){
+		if( 0 == i1 %2){
+ 		    sum1 += i1;
+		}
+		i1++;
+	    }
+	    System.out.println(sum1);
+	   
+	    do {
+		if (0== i2 % 2){
+		    sum2 += i2;
+	  	}
+		i2++;
+	    }while(i2 <= 1000);
+	    System.out.println(sum2);
+	}
+}
