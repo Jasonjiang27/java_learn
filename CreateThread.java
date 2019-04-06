15961c4cabeb7318a3c715ca919532aa9692e971
Jasonjiang27 <jiangyz1991@163.com>
2019-04-06 18:34:25 +0800

    多线程 Thread Runnable

---
+++ CreateThread.java
@@ -0,0 +1,26 @@
+public class CreateThread {
+    public static void main(String[] args){
+        Thread1 thread1 = new Thread1();
+
+        Thread thread2 = new Thread(new Thread2());
+        thread1.start();
+        thread2.start();
+    }
+}
+
+class Thread1 extends Thread{
+    public void run() {
+        for (int i=0;i<100;i++){
+            System.out.println("hello this is "+i);
+
+        }
+    }
+}
+
+class Thread2 implements Runnable {
+    public void run(){
+        for (int i=0;i<100; ++i){
+            System.out.println("Thanks. There is " + i);
+        }
+    }
+}
